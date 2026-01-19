8182197a2b7c5d93b9d9e5ca110098127112c250
Ray <lhy131120@gmail.com>
2026-01-19 17:38:00 +0800

    test commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import process from 'process'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === "production" ? "/kitchen-trabeler/" : "/",
-	plugins: [react(),tailwindcss()],
+	base: process.env.NODE_ENV === "production" ? "/kitchen-traveler/" : "/",
+	plugins: [react(), tailwindcss()],
 });
