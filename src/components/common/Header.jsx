7249562f150fc00679c49db06768c5bc259b397c
ray <lhy131120@gmail.com>
2026-03-03 17:36:40 +0800

    update

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,29 +1,62 @@
 import logo from '@/images/logo.png';
+import "@/style/header.css";
 import { NavLink, Link } from 'react-router';
+import { useRef } from 'react';
+import navRoutes from '@/routes/navRoutes';
 
 export const Header = () => {
+
+	const isMobileMenuOpen = false;
+	const isdropdownOpen = false;
+
+	const message = "週年慶！滿千送百，精美小禮加碼送～";
+	const headerSearchRef = useRef(null);
+
 	return (
 		<>
 			<header className="container">
 				<div className="flex items-center py-5">
+					<button type="button" className="grid lg:hidden place-items-center w-10 h-10 hover:cursor-pointer hover:text-primary-10 transition-colors duration-300"><span class="material-symbols-outlined">menu</span></button>
 					<Link to="/"><img src={logo} alt="logo" className="w-[164px]" /></Link>
-					<nav className='ml-20'>
-						<ul className='list-none flex items-center gap-16'>
-							<li>
-								<NavLink to="/about" className={({ isActive }) => isActive ? 'text-primary-20' : 'text-secondary-50'}>關於我們</NavLink>
-							</li>
-							<li>
-								<NavLink to="/products" className={({ isActive }) => isActive ? 'text-primary-20' : 'text-secondary-50'}>商品列表</NavLink>
-							</li>
-							<li>
-								<NavLink to="/articles" className={({ isActive }) => isActive ? 'text-primary-20' : 'text-secondary-50'}>品牌專欄</NavLink>
-							</li>
-						</ul>
-					</nav>
-					<form className="relative">
-						<input type="text" placeholder="搜尋" className="border border-secondary-60 rounded-lg px-10 py-2" />
-						<span class="material-symbols-outlined block w-6 aspect-square text-lg text-secondary-50">search</span>
-					</form>
+					<div className='flex items-center justify-between flex-1 me-10'>
+						<nav className="">
+							<ul className='mainNav'>
+								<li className='mainNavItem'>
+									<NavLink className={"mainNavLink"} to="/">關於我們</NavLink>
+								</li>
+								<li className='mainNavItem hasSubMenu'>
+									<NavLink className={"mainNavLink"} to="/">商品列表</NavLink>
+									<button type="button" className="w-5 h-5 hover:cursor-pointer">
+										<span className="material-symbols-outlined">keyboard_arrow_down</span>
+									</button>
+									<div className="subNavMenuWrapper">
+										<ul className='subNavMenu'>
+											<li className='subNavItem'><NavLink className={"subNavLink"} to="/">鍋具</NavLink></li>
+											<li className='subNavItem'><NavLink className={"subNavLink"} to="/">刀具</NavLink></li>
+											<li className='subNavItem'><NavLink className={"subNavLink"} to="/">餐具</NavLink></li>
+										</ul>
+									</div>
+								</li>
+								<li className='mainNavItem hasSubMenu'>
+									<NavLink className={"mainNavLink"} to="/">品牌專欄</NavLink>
+									<button type="button" className="w-5 h-5 hover:cursor-pointer">
+										<span className="material-symbols-outlined">keyboard_arrow_down</span>
+									</button>
+									<div className="subNavMenuWrapper">
+										<ul className='subNavMenu'>
+											<li className='subNavItem'><NavLink className={"subNavLink"} to="/">最新消息最新消息</NavLink></li>
+											<li className='subNavItem'><NavLink className={"subNavLink"} to="/">活動公告</NavLink></li>
+											<li className='subNavItem'><NavLink className={"subNavLink"} to="/">品牌故事</NavLink></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+						<form className="relative ml-auto">
+							<input id="headerSearch" ref={headerSearchRef} type="text" placeholder="當季商品熱賣中" className="inputText ps-10.5" />
+							<span className="material-symbols-outlined absolute left-2.5 top-1/2 transform -translate-y-1/2 block w-6 aspect-square text-lg text-secondary-50">search</span>
+						</form>
+					</div>
 					<ul className="list-none flex items-center gap-4 ml-auto">
 						<li>
 							<Link className="flex justify-center items-center w-11 aspect-square text-secondary-50 hover:text-primary-20 duration-300 ease-in-out" to="/cart">
@@ -36,6 +69,8 @@ export const Header = () => {
 					</ul>
 				</div>
 			</header>
+			{/* Promo Message */}
+			<div className="bg-primary-20 text-white text-center py-2 px-4">{message}</div>
 		</>
 	);
 };
--- src/views/front/HomeView.jsx
+++ src/views/front/HomeView.jsx
@@ -1,7 +1,11 @@
 const Home = () => {
 	return (
 		<>
-			<header className=""></header>
+			<button type="button" className="btn">Button</button>
+			<button type="button" className="btn" disabled>Button</button>
+			<button type="button" className="btn btn-outline">Button</button>
+			<button type="button" className="btn btn-icon">立即選購<span class="material-symbols-outlined">arrow_forward_ios</span></button>
+			<button type="button" className="btn btn-outline btn-icon">立即選購<span class="material-symbols-outlined">arrow_forward_ios</span></button>
 		</>
 	);
 };
